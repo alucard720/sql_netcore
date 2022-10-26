c9ee8278bfd267462fab4a358f2dc5e0ddc78862
alucard720 <ing.santana36@gmail.com>
2022-10-26 17:44:47 -0400

    updatemac 7

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:sql_netcore/main.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(
+                "images/Logoblanco.png",
+                fit: BoxFit.contain,
+                height: 35,
+              ),
+              Container(
+                padding: const EdgeInsets.all(8.0),
+                child: const Text('Portal Unico Educacion'),
+              )
+            ],
+          ),
+        ),
+        body: Column(
+          children: [
+            AspectRatio(
+              aspectRatio: 1,
+              child: SizedBox(
+                width: double.infinity,
+                child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2),
+                    itemBuilder: (context, index) {
+                      return const MyApp();
+                    }),
+              ),
+            )
+          ],
+        ));
   }
 }
